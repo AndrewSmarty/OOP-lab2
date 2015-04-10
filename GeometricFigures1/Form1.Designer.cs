4174a06ebacabb5f41ce7416672e8bab9b0bb95e
avmaxim <mi1996xa@mail.ru>
2015-04-10 11:02:26 -0700

    Geometric Shapes Project init

---
+++ GeometricFigures1/Form1.Designer.cs
@@ -0,0 +1,165 @@
+﻿namespace GeometricFigures1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.radioButton4 = new System.Windows.Forms.RadioButton();
+            this.radioButton5 = new System.Windows.Forms.RadioButton();
+            this.radioButton6 = new System.Windows.Forms.RadioButton();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(373, 454);
+            this.panel1.TabIndex = 0;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.radioButton1.Location = new System.Drawing.Point(408, 132);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(155, 35);
+            this.radioButton1.TabIndex = 1;
+            this.radioButton1.Text = "Rectangle";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.radioButton2.Location = new System.Drawing.Point(408, 173);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(102, 35);
+            this.radioButton2.TabIndex = 2;
+            this.radioButton2.Text = "Circle";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged);
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.radioButton3.Location = new System.Drawing.Point(408, 214);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(130, 35);
+            this.radioButton3.TabIndex = 3;
+            this.radioButton3.Text = "Triangle";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            this.radioButton3.CheckedChanged += new System.EventHandler(this.radioButton3_CheckedChanged);
+            // 
+            // radioButton4
+            // 
+            this.radioButton4.AutoSize = true;
+            this.radioButton4.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.radioButton4.Location = new System.Drawing.Point(408, 255);
+            this.radioButton4.Name = "radioButton4";
+            this.radioButton4.Size = new System.Drawing.Size(83, 35);
+            this.radioButton4.TabIndex = 4;
+            this.radioButton4.Text = "Line";
+            this.radioButton4.UseVisualStyleBackColor = true;
+            this.radioButton4.CheckedChanged += new System.EventHandler(this.radioButton4_CheckedChanged);
+            // 
+            // radioButton5
+            // 
+            this.radioButton5.AutoSize = true;
+            this.radioButton5.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.radioButton5.Location = new System.Drawing.Point(408, 296);
+            this.radioButton5.Name = "radioButton5";
+            this.radioButton5.Size = new System.Drawing.Size(119, 35);
+            this.radioButton5.TabIndex = 5;
+            this.radioButton5.Text = "Square";
+            this.radioButton5.UseVisualStyleBackColor = true;
+            this.radioButton5.CheckedChanged += new System.EventHandler(this.radioButton5_CheckedChanged);
+            // 
+            // radioButton6
+            // 
+            this.radioButton6.AutoSize = true;
+            this.radioButton6.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.radioButton6.Location = new System.Drawing.Point(408, 337);
+            this.radioButton6.Name = "radioButton6";
+            this.radioButton6.Size = new System.Drawing.Size(112, 35);
+            this.radioButton6.TabIndex = 6;
+            this.radioButton6.Text = "Ellipse";
+            this.radioButton6.UseVisualStyleBackColor = true;
+            this.radioButton6.CheckedChanged += new System.EventHandler(this.radioButton6_CheckedChanged);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(12, 472);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(373, 33);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "Iterate Figures";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(570, 512);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.radioButton6);
+            this.Controls.Add(this.radioButton5);
+            this.Controls.Add(this.radioButton4);
+            this.Controls.Add(this.radioButton3);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.RadioButton radioButton4;
+        private System.Windows.Forms.RadioButton radioButton5;
+        private System.Windows.Forms.RadioButton radioButton6;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ GeometricFigures1/Form1.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace GeometricFigures1
+{
+    public partial class Form1 : Form
+    {
+        private readonly Rectangle _rect;
+        private readonly Line _line;
+        private readonly Triangle _triangle;
+        private readonly Circle _circle;
+        private readonly Ellipse _ellipse;
+        private readonly Square _square;
+        private readonly Shapes _shapes;
+
+        public Form1()
+        {
+            InitializeComponent();
+            _rect = new Rectangle()
+            _circle = new Circle();
+            _triangle = new Triangle();
+            _line = new Line();
+            _ellipse = new Ellipse();
+            _square = new Square();
+            _shapes = new Shapes();
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            rect.Draw();
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            circle.Draw();
+        }
+
+        private void radioButton3_CheckedChanged(object sender, EventArgs e)
+        {
+            triangle.Draw();
+        }
+
+        private void radioButton4_CheckedChanged(object sender, EventArgs e)
+        {
+            line.Draw();
+        }
+
+        private void radioButton5_CheckedChanged(object sender, EventArgs e)
+        {
+            square.Draw();
+        }
+
+        private void radioButton6_CheckedChanged(object sender, EventArgs e)
+        {
+            ellipse.Draw();
+        }
+
+        private void radioButton7_CheckedChanged(object sender, EventArgs e)
+        {
+          
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            rect
+            rect.SetPosition(80, 80);
+            rect.SetDimensions(200, 300);
+            rect.Surface = panel1;
+
+
+            circle.SetPosition(80, 120);
+            circle.SetRadius(100);
+            circle.Surface = panel1;
+
+
+            square.SetSide(200);
+            square.Surface = panel1;
+            square.SetPosition(100, 100);
+
+
+            ellipse.SetPosition(80, 80);
+            ellipse.SetDimensions(200, 300);
+            ellipse.Surface = panel1;
+            
+
+            triangle.SetPoints(new Point[]{
+                                    new Point(100, 300), 
+                                    new Point(300, 300), 
+                                    new Point(200, 100)
+                                });
+            triangle.Surface = panel1;
+
+
+            line.SetPoints(new Point[]{
+                                    new Point(80, 80), 
+                                    new Point(280, 380)
+                                });
+            line.Surface = panel1;
+
+
+            _shapes.AddFigure(rect);
+            _shapes.AddFigure(circle);
+            _shapes.AddFigure(ellipse);
+            _shapes.AddFigure(triangle);
+            _shapes.AddFigure(line);
+            _shapes.AddFigure(square);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            _shapes.Draw();
+        }
+    }
+}
